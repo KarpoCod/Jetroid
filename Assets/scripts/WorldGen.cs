eceaf165d93fe862a790b8ba24eaf808c8196664
KarpoCod <2oivit160807@gmail.com>
2023-08-20 17:17:27 +0300

    17.17

--- Assets/scripts/WorldGen.cs
+++ Assets/scripts/WorldGen.cs
@@ -13,7 +13,6 @@ public class WorldGen : MonoBehaviour
     public Dictionary<Vector2Int, ChunkData> ChunkDatas = new Dictionary<Vector2Int, ChunkData>();
 
     public ChunkRenderer ChunkPrefab;
-    public GameObject Cursor;
     public GameObject Player;
     public GameObject World;
     public int seed;
@@ -113,8 +112,8 @@ public class WorldGen : MonoBehaviour
         
         if (hitInfo != false)
         {
-            Cursor.SetActive(true);
-            Cursor.transform.position = Vector3Int.FloorToInt(hitInfo.point);
+            //Cursor.SetActive(true);
+            //Cursor.transform.position = Vector3Int.FloorToInt(hitInfo.point);
             Debug.DrawRay(PPos, hitP, Color.yellow);
             if (Input.GetButton("Fire1") && CD > digCD)
             {
@@ -149,7 +148,7 @@ public class WorldGen : MonoBehaviour
         }
         else
         {
-            Cursor.SetActive(false);
+            //Cursor.SetActive(false);
             Debug.DrawRay(PPos, mouse, Color.red);
         }
     }
