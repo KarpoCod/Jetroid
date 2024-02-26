0c52315350c8aa40626214cd70a5598a00c77e78
artemidka228 <artemdg06@gmail.com>
2024-02-26 22:56:11 +0300

    22 56

--- Assets/scripts/Player/Character.cs
+++ Assets/scripts/Player/Character.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField] private int Health = 100;
+    [SerializeField] public static int Health = 100;
     public float speed = 7;
     public float jumpVelocity = 5f;
     public float jetSpeed = 5f;
@@ -40,7 +40,7 @@ public class Character : MonoBehaviour
         body2D.velocity = new Vector2(VelosX, (standing) ? 0 : body2D.velocity.y);
         if (standing && Direction != 0) { animator.SetInteger("AnimState", 1); }
         else if (standing) { animator.SetInteger("AnimState", 0); }
-        else { animator.SetInteger("AnimState", 3); }
+        else { animator.SetInteger("AnimState", 2); }
         JetPack.Fly(false);
     }
 
@@ -61,7 +61,7 @@ public class Character : MonoBehaviour
         float VelosX = boostMultiplier * Direction * speed;
         if (Mathf.Abs(VelosX) < Mathf.Abs(body2D.velocity.x) && body2D.velocity.x / VelosX > 0) { VelosX = body2D.velocity.x; }
         body2D.velocity = new Vector2(VelosX, (standing) ? 0 : body2D.velocity.y);
-        animator.SetInteger("AnimState", 4);
+        animator.SetInteger("AnimState", 3);
         JetPack.Fly(true);
     }
 }
--- Assets/scripts/Player/JetBar.cs
+++ Assets/scripts/Player/JetBar.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 public class JetBar : MonoBehaviour
 {
     public Image jetBar;
+    public Image HPBar;
 
     void Update()
     {
         jetBar.fillAmount = PlayerController.fuel;
+        HPBar.fillAmount = Character.Health;
     }
 }
