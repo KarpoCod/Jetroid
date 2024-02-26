4df5cfdd6225e38e7f34e34288cf32854bc8912b
KarpoCod <2oivit160807@gmail.com>
2024-02-26 13:22:30 +0300

    Player controller ready

--- Assets/scripts/Player/Character.cs
+++ Assets/scripts/Player/Character.cs
@@ -7,12 +7,14 @@ public class Character : MonoBehaviour
     [SerializeField] private int Health = 100;
     public float speed = 7;
     public float jumpVelocity = 5f;
-    public float jetSpeed = 50f;
+    public float jetSpeed = 5f;
     public float boostMultiplier = 3f;
     public float airMultiplier = 0.5f;
     public bool canFly = true;
     private Animator animator;
     private Rigidbody2D body2D;
+
+    private float VelosX = 0;
 
     void Start()
     {
@@ -25,9 +27,16 @@ public class Character : MonoBehaviour
         if (Direction != 0)
         {
             transform.localScale = new Vector3((Mathf.Abs(Direction) / Direction) * Mathf.Abs(transform.localScale.x), 1 * transform.localScale.y, 1);
+            VelosX = ((standing) ? 1 : airMultiplier) * Direction * speed;
+            if (Mathf.Abs(VelosX) < Mathf.Abs(body2D.velocity.x) && body2D.velocity.x/VelosX > 0) { VelosX = body2D.velocity.x; }
         }
-        float VelosX = ((standing) ? 1 : airMultiplier) * Direction * speed;
-        body2D.velocity = new Vector2((Mathf.Abs(VelosX) > Mathf.Abs(body2D.velocity.x)) ? VelosX : body2D.velocity.x, (standing) ? 0 : body2D.velocity.y);
+        else if (Mathf.Abs(body2D.velocity.x) < 0.7 * speed)
+        {
+            VelosX = 0;
+        }
+        else { VelosX = body2D.velocity.x; }
+
+        body2D.velocity = new Vector2(VelosX, (standing) ? 0 : body2D.velocity.y);
         if (standing && Direction != 0) { animator.SetInteger("AnimState", 1); }
         else if (standing) { animator.SetInteger("AnimState", 0); }
         else { animator.SetInteger("AnimState", 3); }
@@ -37,7 +46,8 @@ public class Character : MonoBehaviour
     {
         if (Direction != 0) { transform.localScale = new Vector3((Mathf.Abs(Direction) / Direction) * Mathf.Abs(transform.localScale.x), 1 * transform.localScale.y, 1); }
         float VelosX = airMultiplier * Direction * speed;
-        body2D.velocity = new Vector2((Mathf.Abs(VelosX) > Mathf.Abs(body2D.velocity.x)) ? VelosX : body2D.velocity.x, jetSpeed);
+        if (Mathf.Abs(VelosX) < Mathf.Abs(body2D.velocity.x) && body2D.velocity.x / VelosX > 0) { VelosX = body2D.velocity.x; }
+        body2D.velocity = new Vector2(VelosX, jetSpeed);
         animator.SetInteger("AnimState", 2);
     }
 
@@ -46,7 +56,8 @@ public class Character : MonoBehaviour
     {
         transform.localScale = new Vector3((Mathf.Abs(Direction) / Direction) * Mathf.Abs(transform.localScale.x), 1 * transform.localScale.y, 1);
         float VelosX = boostMultiplier * Direction * speed;
-        body2D.velocity = new Vector2((Mathf.Abs(VelosX) > Mathf.Abs(body2D.velocity.x)) ? VelosX : body2D.velocity.x, (standing) ? 0 : body2D.velocity.y);
+        if (Mathf.Abs(VelosX) < Mathf.Abs(body2D.velocity.x) && body2D.velocity.x / VelosX > 0) { VelosX = body2D.velocity.x; }
+        body2D.velocity = new Vector2(VelosX, (standing) ? 0 : body2D.velocity.y);
         animator.SetInteger("AnimState", 4);
     }
 }
--- Assets/scripts/Player/Inventory/ItemClass.cs
+++ Assets/scripts/Player/Inventory/ItemClass.cs
@@ -1,6 +1,6 @@
 public enum ItemClass : byte
 {
     block,
-    jet,
+    equipment,
     tool
 }
