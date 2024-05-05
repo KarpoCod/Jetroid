e68275a559136281d70fb9126816b3651d62f7f4
artemidka228 <artemdg06@gmail.com>
2024-05-05 19:55:19 +0300

    19:54 5 may 2024

---
+++ Assets/scripts/Inventory/InventoryOpen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryOpen : MonoBehaviour
+{
+    public GameObject[] Controlled_obj;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            foreach (GameObject go in Controlled_obj) { go.SetActive(!go.activeSelf); }
+        }
+    }
+}
