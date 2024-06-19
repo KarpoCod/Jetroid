4b16b4e8d27ba4b0769677c04e73c397b0a9f554
artemidka228 <artemdg06@gmail.com>
2024-06-19 23:43:16 +0300

    pochti nifiga ne sdelal, no GUI UPDATE

--- Assets/scripts/ChooseWrldButtManager.cs
+++ Assets/scripts/ChooseWrldButtManager.cs
@@ -8,6 +8,7 @@ public class ChooseWrldButtManager : MonoBehaviour
     public GameObject button;
     public GameObject EditName;
     public GameObject Delete;
+    public GameObject Info;
     BoxCollider2D Collider;
 
     void Start()
@@ -20,6 +21,7 @@ public class ChooseWrldButtManager : MonoBehaviour
         //Debug.Log("asdasd");
         EditName.SetActive(true);
         Delete.SetActive(true);
+        Info.SetActive(true);
         Collider.size = new Vector2(628, 100);
     }
 
@@ -27,6 +29,22 @@ public class ChooseWrldButtManager : MonoBehaviour
     {
         EditName.SetActive(false);
         Delete.SetActive(false);
+        Info.SetActive(false);
         Collider.size = new Vector3(480, 100);
+    }
+
+    public void Edit()
+    {
+        Debug.Log("Edit");
+    }
+
+    public void DeleteWorld()
+    {
+        Debug.Log("DeleteWorld");
+    }
+
+    public void WorldInfo()
+    {
+        Debug.Log("WorldInfo");
     }
 }
